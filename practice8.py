7d007c584f66481bcf948e85fc143da4ddf8ab81
Swapnil Khambayat <38365464+swapnilkhambayat@users.noreply.github.com>
2020-08-09 22:19:49 +1000

    commit

--- practice8.py
+++ practice8.py
@@ -7,19 +7,23 @@ fifth_value = 'fifth'
 sixth_value = 'sixth'
 
 # First challenge
-first_value = (first_value.title()).strip()
+first_value = (first_value.title().strip(' '))
+second_value = (second_value.strip('-')).strip(' ').capitalize()
+third_value = (third_value.lower()).replace(' ','').replace('-',' ').capitalize()
 
-# Second challenge
-
-
-# Third challenge
-
-
-print(first_value)
+print(f'{first_value:^30}')
 print(second_value)
-print(third_value)
+print(f'{third_value:>30}')
 
 # Fourth challenge - use only the print() function (no f-strings)
-
+print(fourth_value +'#'+fifth_value+'#'+sixth_value+'!')
 
 # Fifth challenge - use only a single print() function.  Create tabs and new lines using f-strings.
+print(f'{fourth_value:>13} \n {fifth_value:>11} \n {sixth_value:>11}')
+#       First Challenge        
+#Second challenge
+#               Third challenge
+#fourth#fifth#sixth!
+#        fourth
+#        fifth
+#        sixth
